387f2721b3b4e2112f024147e6525911bf04e027
Denis Toloshny <gg.zyus@gmail.com>
2016-04-01 16:04:22 +0300

    style and some lint

--- js/index.js
+++ js/index.js
@@ -12,34 +12,30 @@ $('#se-pre-con').bind('ajaxStart', function(){
 //load first batch from api
 $(document).ready(function(){
   $.getJSON('http://pokeapi.co/api/v1/pokemon/?limit=12', function(data){
-    console.log(data)
-    processData(data);  
+    //console.log(data);
+    processData(data);
     //turnoff preloader
     $(".se-pre-con").fadeOut(200);
-
-  })        
+  })
 });
 
 // Show blocks of 12 pokemons, with onclick to show details about
 function processData(data){
   for (var i=0; i<data.objects.length; i++){
-  
    var link = $('<a href="#'+data.objects[i].name+'"></a>');
    var img = $('<img src="http://toloshny.com/pokeimg/'+getNormalizedNumber(data.objects[i].pkdx_id)+'.png" class="pokepic">');
    var poceblock = $('<div class="col-sm-4 col-xs-12 col-md-3 pokenames text-center"></div>');
-   var poceblocktext =$('<br>'+data.objects[i].name+'<br><span class="types">'+getPoceTypesWithStyling(data.objects[i])+'</span>'); 
+   var poceblocktext =$('<br>'+data.objects[i].name+'<br><span class="types">'+getPoceTypesWithStyling(data.objects[i])+'</span>');
 
    link.append(img); 
    link.click((function(e){
     var poceObject = data.objects[i];
-    
 
     return function() {
       var img = '<img src="http://toloshny.com/pokeimg/'+getNormalizedNumber(poceObject.pkdx_id)+'.png" class="poce-details-image">';
       var pokedetails = $('<div class="">'+img+'<br><span class="bold">'+poceObject.name+'</span><br> Type: '+getPoceTypes(poceObject)+
        '<br> Attack: '+poceObject.attack+'<br> Defence: '+poceObject.defense+'<br> Health: '+poceObject.hp+'<br> SP Attack:  '+poceObject.sp_atk+'<br> SP Defense:  '+poceObject.sp_def+'<br> Speed:  '+poceObject.speed+'<br> Weight:  '+poceObject.weight+'<br> Total moves:  '+poceObject.moves.length+'<br></div>');
       $('.pokedetails').hide().html(pokedetails).addClass('pokedetails-border').fadeIn(300);
-
 
       return false;
     }
@@ -60,29 +56,28 @@ function getPoceTypesWithStyling(poceObject){
   for (var count=0;count<poceObject.types.length; count++){
     var typeName=poceObject.types[count].name;
     poceTypes+="<span class='"+typeName+"-color type-padding' >"+typeName+" </span>";
- 
- }
- 
- return poceTypes;
+
+  }
+  return poceTypes;
 }
 
 function getPoceTypes(poceObject){
   var poceTypes = "";
   for (var count=0;count<poceObject.types.length; count++){
-      poceTypes+=poceObject.types[count].name+" ";
-   } 
- return poceTypes;
+    poceTypes+=poceObject.types[count].name+" ";
+  } 
+  return poceTypes;
 }
 
 //Load new bunch of poceapi by buttonclikc and process it
 $('.loadmore-btn').click(function(){
   //add preloader
   $.getJSON('http://pokeapi.co/api/v1/pokemon/?limit=12&offset='+offsetNum, function(data2){
-    console.log(data2)
-    processData(data2); 
+    //console.log(data2)
+    processData(data2);
     //stop preloader
   })  
-  offsetNum +=12; 
+  offsetNum +=12;
 });
 
 // Normalize ID pockemon number to make it 3 digits starting with zeroes like 000.png
